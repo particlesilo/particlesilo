c0d71520701fb6dd20aaf34b073ee2b70b9a3301
Manya Agarwal <manya18ag@gmail.com>
2021-05-15 16:00:38 +0800

    web: style search input

--- web/src/components/Home.js
+++ web/src/components/Home.js
@@ -8,9 +8,9 @@ export const Home = () => {
     setSearchQuery(e.target.value);
   };
   return (
-    <div className="w-1/3 mx-auto">
-      <img src={logo} className="object-scale-down" />
-      <p className="text-gray-50 text-3xl text-center">
+    <div className="w-3/5 mx-auto items-center justify-items-center">
+      <img src={logo} className="object-scale-down w-1/2 mx-auto" />
+      <p className="text-gray-50 text-xl text-center">
         The best Science Communication materials for your next outreach
         activities!
       </p>
--- web/src/components/utils/SearchInput.js
+++ web/src/components/utils/SearchInput.js
@@ -1,16 +1,19 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 export const SearchInput = ({ name, placeholder, value, onChange}) => {
   return (
-    <div className="mb-3 pt-0">
+    <div className="relative flex w-full flex-wrap items-stretch mb-3 p-8">
       <input
         type="text"
-        className="px-3 py-4 placeholder-blueGray-300 text-gray-50 border-b-2 bg-white bg-opacity-0 focus:border-red-400 "
+        className="w-11/12 px-3 text-2xl py-3 placeholder-blueGray-300 text-gray-50 border-0 border-b-2 border-opacity-40 bg-white bg-opacity-0 focus:border-red-400 "
         name={name}
         placeholder={placeholder}
         value={value}
         onChange={onChange}
       />
+      <FontAwesomeIcon icon={faSearch} className="text-gray-500 text-xl z-10 h-full leading-snug text-center text-base items-center justify-center py-3 w-8"/>
     </div>
   );
 };
