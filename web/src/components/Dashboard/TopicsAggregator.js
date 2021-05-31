9388b50648aeb32304ed42a7a3db241cf2dae77e
uzzielperez <uzzie.perez@cern.ch>
2021-05-31 18:32:19 +0200

    backup

--- web/src/components/Dashboard/TopicsAggregator.js
+++ web/src/components/Dashboard/TopicsAggregator.js
@@ -2,5 +2,16 @@ import React from 'react'
 import { styleClass } from '../../utils/style_class'
 
 export const TopicsAggregator = (topics) => {
-  return <div className="rounded-lg bg-gray-300 w-auto h-96">Topics</div>
+  return (
+    //
+    <div className="rounded-lg bg-gray-300 w-auto h-96">
+      <div class="grid grid-cols-1 md:grid-cols-4">
+        <button type="button" onclick="alert('Create Topics')">Topics</button>
+
+        <div class="mt-12 flex justify-center">
+          <div class="btn bg-secondary-100 text-secondary-200">Load more</div>
+        </div>
+      </div>
+    </div>
+  )
 };
