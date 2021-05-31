403d00dc00abc93732616fbf051d42e56718f230
uzzielperez <uzzie.perez@cern.ch>
2021-05-31 15:16:22 +0200

    Experimental Dashboard arranement

--- web/src/components/Dashboard/TopicsAggregator.js
+++ web/src/components/Dashboard/TopicsAggregator.js
@@ -2,5 +2,5 @@ import React from 'react'
 import { styleClass } from '../../utils/style_class'
 
 export const TopicsAggregator = (topics) => {
-  return <div className="rounded-lg bg-gray-300 w-72 h-96">Topics</div>
+  return <div className="rounded-lg bg-gray-300 w-auto h-96">Topics</div>
 };
