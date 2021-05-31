8f47cdc2868e1a8fa12e3f38f7bc20299dbcefe3
uzzielperez <uzzie.perez@cern.ch>
2021-05-31 11:08:15 +0200

    Dashboard team

---
+++ web/src/components/Dashboard/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { styleClass } from '../utils/style_class'
+
+export const Dashboard = () => {
+    return(
+        <div className={styleClass.container}>
+        </div>
+    )
+}
