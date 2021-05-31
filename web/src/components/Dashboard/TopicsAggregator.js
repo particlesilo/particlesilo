459bc7cdf811293267b96ff917765fe37ac0616c
uzzielperez <uzzie.perez@cern.ch>
2021-05-31 14:31:45 +0200

    working bas

---
+++ web/src/components/Dashboard/TopicsAggregator.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { styleClass } from '../../utils/style_class'
+
+export const TopicsAggregator = (topics) => {
+  return <div className="rounded-lg bg-gray-300 w-72 h-96">Topics</div>
+};
