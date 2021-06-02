ae3702ce9d8f910ab78728b707ab5070b7f80a7a
uzzielperez <uzzie.perez@cern.ch>
2021-06-02 22:59:01 +0200

    list of Topics

--- web/src/components/Dashboard/TopicsAggregator.js
+++ web/src/components/Dashboard/TopicsAggregator.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react';
 import { styleClass } from '../../utils/style_class';
 import { Link } from "react-router-dom";
 
-export const TopicsAggregator = ({topics}) => {
+export const TopicsAggregator = ({topicsAgg}) => {
+
+  //const [todos, setTodos] = useState([]);
+
+  const topics = ['Rolling in the Higgs', 'What is a Discovery?', 'Dark Matter Web']
+  const topicsList = <Link to="/topic/example"> {topics.map(topics => <button className="rounded-lg bg-purple-600 hover:bg-green-500 py-2 px-4 text-white">{topics}</button>)}</Link>
+
   return <div className="rounded-lg bg-gray-300 w-auto h-96">
     <div>
       <button className="rounded-lg border-red-700 bg-gray-400 hover:bg-green-500 py-4 px-8 text-white">Favorite Topics</button>
     </div>
     <div>
-      <Link to="/topic/example"> <button className="rounded-lg bg-purple-600 hover:bg-green-500 py-2 px-4 text-white">Rolling in the higgs</button></Link>
+      {topicsList}
     </div>
 
     <div>
