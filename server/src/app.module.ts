53c541321e8afa56afd864d2ff7921345bc5f55a
Manya Agarwal <manya18ag@gmail.com>
2021-06-03 01:02:29 +0800

    server: add database docker image and typeorm

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { configService } from './config/config.service';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule, TypeOrmModule.forRoot(configService.getTypeOrmConfig()),],
   controllers: [AppController],
   providers: [AppService],
 })
