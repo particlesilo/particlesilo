232ad6daf531e567462950d5e2d069506a4ff199
uzzielperez <uzzie.perez@cern.ch>
2021-06-04 14:47:08 +0200

    topics

---
+++ web/src/components/Dashboard/topicsComponents/topicsForm.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+function topicsForm() {
+  const [topics, setTopics] = useState({
+    id: "",
+    description: ""
+  });
+
+  return (
+    <form>
+      <input />
+      <button />
+    </form>
+  );
+}
+
+export default topicsForm;
