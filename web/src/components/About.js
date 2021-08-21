9326e5d935c9e1ce178f15ef5a48e8e6abd92e4a
uzzielperez <uzzie.perez@cern.ch>
2021-08-21 19:14:41 +0200

    About

--- web/src/components/About.js
+++ web/src/components/About.js
@@ -3,8 +3,8 @@ import 'react-slideshow-image/dist/styles.css'
 import { Slide } from 'react-slideshow-image';
 
 const slideImages = [
-  'https://cds.cern.ch/record/2461231/files/icon1440-2002-06-020_X_CERN_00677_0002.jpg?subformat=icon-1440',
   'https://cds.cern.ch/record/2777102/files/HIG-20-005_0.png?version=1',
+  'https://cds.cern.ch/record/2461231/files/icon1440-2002-06-020_X_CERN_00677_0002.jpg?subformat=icon-1440',
   'https://cds.cern.ch/record/2230893/files/HiggsWW.gif?subformat=icon-1440',
 ];
 
